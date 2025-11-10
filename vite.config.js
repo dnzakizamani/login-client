c8c462875cfae9773146a0ad866b57141f782596
Zaki <zakizamaniaza@gmail.com>
2025-11-10 11:52:13 +0700

    setting vercel

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-          'router-vendor': ['react-router-dom'],
-          'ui-vendor': ['react-icons', 'react-toastify'],
-          'pdf-vendor': ['react-pdf', 'pdfjs-dist']
-        }
-      }
-    }
+    // sourcemap: true,
+    // minify: 'terser',
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks: {
+    //       'react-vendor': ['react', 'react-dom'],
+    //       'router-vendor': ['react-router-dom'],
+    //       'ui-vendor': ['react-icons', 'react-toastify'],
+    //       'pdf-vendor': ['react-pdf', 'pdfjs-dist']
+    //     }
+    //   }
+    // }
   }
 })
