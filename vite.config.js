3a5fb5986521bf36f45fde1db69db328c2982b84
Zaki <zakizamaniaza@gmail.com>
2025-11-10 11:25:06 +0700

    Fix Vite config and Vercel deployment settings

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'router-vendor': ['react-router-dom'],
+          'ui-vendor': ['react-icons', 'react-toastify'],
+          'pdf-vendor': ['react-pdf', 'pdfjs-dist']
+        }
+      }
+    }
+  }
 })
